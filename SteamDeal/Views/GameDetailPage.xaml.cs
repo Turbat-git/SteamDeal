e132e3a2a63a65106bd67e107c241d620b5e6223
Tom <20136824@tafe.wa.edu.au>
2025-06-17 19:32:06 +0800

    feat: init

--- SteamDeal/Views/GameDetailPage.xaml.cs
+++ SteamDeal/Views/GameDetailPage.xaml.cs
@@ -2,36 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using SteamDeal.Models;
 using SteamDeal.ViewModels;
 
 namespace SteamDeal.Views
 {
+    [QueryProperty(nameof(SerializedGame), "SelectedGame")]
     public partial class GameDetailPage : ContentPage
     {
-        public GameDetailPage(GameDeal selectedGame)
+        private GameDeal _selectedGame;
+
+        public string SerializedGame
+        {
+            get => null;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    try
+                    {
+                        _selectedGame = JsonSerializer.Deserialize<GameDeal>(Uri.UnescapeDataString(value));
+                        BindingContext = new GameDetailViewModel(_selectedGame);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error deserializing game: {ex.Message}");
+                    }
+                }
+            }
+        }
+
+        public GameDetailPage()
         {
             InitializeComponent();
-            BindingContext = new GameDetailViewModel(selectedGame);
         }
 
         private void OnAddToWishlistClicked(object sender, EventArgs e)
         {
             // TODO: Add logic for wishlisting a game
             DisplayAlert("Wishlist", "Game added to your wishlist!", "OK");
-        }
-
-        private GameDeal _selectedGame;
-
-        public GameDeal SelectedGame
-        {
-            get => _selectedGame;
-            set
-            {
-                _selectedGame = value;
-                BindingContext = new GameDetailViewModel(_selectedGame);
-            }
         }
     }
 }
