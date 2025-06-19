eff6398388d2d59407f176fc6fcdf9866517cf66
Tom <20136824@tafe.wa.edu.au>
2025-06-19 20:23:21 +0800

    feat: add app theme to preferences

--- SteamDeal/App.xaml.cs
+++ SteamDeal/App.xaml.cs
@@ -7,6 +7,14 @@
             InitializeComponent();
 
             MainPage = new AppShell();
+
+            if (Preferences.ContainsKey("UserTheme"))
+            {
+                var theme = Preferences.Get("UserTheme", "Unspecified");
+                App.Current.UserAppTheme = Enum.TryParse<AppTheme>(theme, out var parsedTheme)
+                    ? parsedTheme
+                    : AppTheme.Unspecified;
+            }
         }
     }
 }
--- SteamDeal/Views/SettingsPage.xaml.cs
+++ SteamDeal/Views/SettingsPage.xaml.cs
@@ -5,7 +5,17 @@ public partial class SettingsPage : ContentPage
 	public SettingsPage()
 	{
 		InitializeComponent();
-	}
+        ThemeSwitch.IsToggled = App.Current.UserAppTheme == AppTheme.Dark;
+    }
+
+    private void OnThemeToggled(object sender, ToggledEventArgs e)
+    {
+        var newTheme = e.Value ? AppTheme.Dark : AppTheme.Light;
+        App.Current.UserAppTheme = newTheme;
+
+        Preferences.Set("UserTheme", newTheme.ToString());
+
+    }
     private async void OnSteamLogoClicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("//MainPage");
