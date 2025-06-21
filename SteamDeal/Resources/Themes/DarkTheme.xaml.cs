4d6a549a8490841f6ab9dff08f40454dd7b19a42
Tom <20136824@tafe.wa.edu.au>
2025-06-21 18:49:19 +0800

    feat: add steam and search logo to Shell Titleview

--- SteamDeal/Resources/Themes/DarkTheme.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace SteamDeal.Resources.Styles;
-
-public partial class DarkTheme : ResourceDictionary
-{
-	public DarkTheme()
-	{
-		InitializeComponent();
-	}
-}
--- SteamDeal/Resources/Themes/LightTheme.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace SteamDeal.Resources.Styles;
-
-public partial class LightTheme : ResourceDictionary
-{
-	public LightTheme()
-	{
-		InitializeComponent();
-	}
-}
