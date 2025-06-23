1bafeadae7f6bf84b0ad2ffe252cebc18ab58aa4
Tom <20136824@tafe.wa.edu.au>
2025-06-23 09:41:12 +0800

    chore: get rid of useless data in api response

--- SteamDeal/Models/GameDetailResponse.cs
+++ SteamDeal/Models/GameDetailResponse.cs
@@ -6,27 +6,15 @@ public class GameDetailResponse
 {
     [JsonPropertyName("gameInfo")]
     public GameInfo GameInfo { get; set; }
-
-    [JsonPropertyName("cheaperStores")]
-    public object[] CheaperStores { get; set; }
-
-    [JsonPropertyName("cheapestPrice")]
-    public CheapestPrice CheapestPrice { get; set; }
 }
 
 public class GameInfo
 {
-    [JsonPropertyName("storeID")]
-    public string StoreID { get; set; }
-
     [JsonPropertyName("gameID")]
     public string GameID { get; set; }
 
     [JsonPropertyName("name")]
     public string Name { get; set; }
-
-    [JsonPropertyName("steamAppID")]
-    public string SteamAppID { get; set; }
 
     [JsonPropertyName("thumb")]
     public string Thumb { get; set; }
@@ -37,36 +25,9 @@ public class GameInfo
     [JsonPropertyName("retailPrice")]
     public string RetailPrice { get; set; }
 
-    [JsonPropertyName("steamRatingText")]
-    public string SteamRatingText { get; set; }
-
-    [JsonPropertyName("steamRatingPercent")]
-    public string SteamRatingPercent { get; set; }
-
-    [JsonPropertyName("steamRatingCount")]
-    public string SteamRatingCount { get; set; }
-
-    [JsonPropertyName("metacriticScore")]
-    public string MetacriticScore { get; set; }
-
-    [JsonPropertyName("metacriticLink")]
-    public string MetacriticLink { get; set; }
-
-    [JsonPropertyName("releaseDate")]
-    public long ReleaseDate { get; set; }
-
-    [JsonPropertyName("publisher")]
-    public string Publisher { get; set; }
-
-    [JsonPropertyName("steamworks")]
-    public object Steamworks { get; set; }
-
     // Legacy properties for backward compatibility
     [JsonIgnore]
     public string Title => Name;
-
-    [JsonIgnore]
-    public string DealRating => "0"; // Not provided in detail API
 
     [JsonIgnore]
     public string NormalPrice => RetailPrice;
@@ -89,10 +50,4 @@ public class GameInfo
 
     [JsonIgnore]
     public string DealID { get; set; } // This will be set separately when needed
-}
-
-public class CheapestPrice
-{
-    [JsonPropertyName("date")]
-    public long Date { get; set; }
 }
