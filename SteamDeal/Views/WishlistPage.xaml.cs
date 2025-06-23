edec794ecaa68690fbb18377f97a3e695036e7c0
Tom <20136824@tafe.wa.edu.au>
2025-06-23 09:56:13 +0800

    feat: add more

--- SteamDeal/Views/WishlistPage.xaml.cs
+++ SteamDeal/Views/WishlistPage.xaml.cs
@@ -1,15 +1,25 @@
-﻿namespace SteamDeal.Views;
+﻿using SteamDeal.ViewModels;
+
+namespace SteamDeal.Views;
 
 public partial class WishlistPage : ContentPage
 {
-    public WishlistPage()
+    public WishlistPage(WishlistViewModel viewModel)
     {
         InitializeComponent();
+        BindingContext = viewModel;
     }
+
     private async void OnSteamLogoClicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("//MainPage");
     }
+
+    private async void OnBrowseDealsClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//MainPage");
+    }
+
     public void OnHamburgerClicked(object sender, EventArgs e)
     {
         Shell.Current.FlyoutIsPresented = true;
